65bb3cc9fe1c15f925394be43aac788e223db365
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-28 23:05:03 +0200

    Update authRoutes.js

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -11,9 +11,9 @@ const {
 dotenv.config();
 const router = express.Router();
 
-router.post("/auth/register", register);
-router.post("/auth/login", login);
-router.post("/auth/refresh", refresh);
-router.post("/auth/logout", authMiddleware, logout);
+router.post("/register", register); // Trasa dla rejestracji
+router.post("/login", login); // Trasa dla logowania
+router.post("/refresh", refresh); // Trasa do odświeżania tokenu
+router.post("/logout", authMiddleware, logout); // Trasa dla wylogowania
 
 module.exports = router;
