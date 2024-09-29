bed79bd83d310c911968de4b3d1fbbb14ce7ac2f
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-29 22:04:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,24 +15,15 @@ dotenv.config();
 
 const loggerFormats = app.get("env") === "development" ? "dev" : "short";
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://projekt-kapusta-frontend.vercel.app",
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://projekt-kapusta-frontend.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
-app.use(cors(corsOptions));
-app.options("/api/login", cors(corsOptions));
+
+app.use(cors(corsOptions)); // Używamy CORS z określonymi opcjami
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -59,4 +50,3 @@ app.use((err, _req, res, next) => {
 });
 
 module.exports = app;
-// DDD
