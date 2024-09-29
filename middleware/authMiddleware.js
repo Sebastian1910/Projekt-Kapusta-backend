35e169ee70efcd7ebb4ed7839058f4918d32efad
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-29 23:03:36 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const User = require("../models/User.js");
+
 const authMiddleware = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, async (error, user) => {
     if (!user || error) {
@@ -10,7 +11,20 @@ const authMiddleware = (req, res, next) => {
       });
     }
     try {
-      const token = req.header("Authorization").replace("Bearer ", "");
+      const authHeader = req.header("Authorization");
+
+      // Sprawdź, czy nagłówek Authorization istnieje i jest poprawnie sformatowany
+      if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({
+          status: "401 Unauthorized",
+          code: 401,
+          message: "Token is missing or invalid format",
+        });
+      }
+
+      const token = authHeader.replace("Bearer ", "");
+
+      // Sprawdź, czy token jest zgodny z tokenem użytkownika w bazie danych
       const isUser = await User.findOne({
         _id: user._id,
         token: token,
@@ -18,11 +32,13 @@ const authMiddleware = (req, res, next) => {
 
       if (!isUser) {
         return res.status(401).json({
-          status: "401 Unauthorization",
+          status: "401 Unauthorized",
           code: 401,
           message: "Token is not valid",
         });
       }
+
+      // Przypisz użytkownika do req
       req.user = isUser;
       next();
     } catch (error) {
