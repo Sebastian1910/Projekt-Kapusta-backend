35c809cdf607e0aa85884f5ad655c9c13b8f6b76
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-29 11:01:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Projekt-Kapusta-backend/",
+  // base: "/Projekt-Kapusta-backend/",
   build: {
     outDir: "./dist",
   },
