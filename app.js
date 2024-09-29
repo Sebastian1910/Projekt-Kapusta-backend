43f718e0be8e694b1d77e5377f79027695712187
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-29 19:14:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/api/login", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
