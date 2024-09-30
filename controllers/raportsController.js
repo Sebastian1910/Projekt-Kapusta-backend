b7ae06c67b08912c07cafe5b8c7de6360b36fb9c
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-30 10:24:52 +0200

    Update raportsController.js

--- controllers/raportsController.js
+++ controllers/raportsController.js
@@ -4,29 +4,47 @@ const {
   expenseCategorie,
 } = require("../config/categories.js");
 
-const getPeriodData = async (startDate, endDate) => {
+// Funkcja do pobierania danych raportów z bazy danych
+const getReportsFromDatabase = async (year, month) => {
+  const startDate = new Date(year, month - 1, 1); // Pierwszy dzień miesiąca
+  const endDate = new Date(year, month, 0); // Ostatni dzień miesiąca
+
+  endDate.setHours(23, 59, 59, 999); // Ustawienie pełnych godzin na koniec dnia
+
   try {
+    // Pobieranie transakcji w danym miesiącu
     const transactions = await Transaction.find({
-      date: { $gte: new Date(startDate), $lte: new Date(endDate) },
+      date: { $gte: startDate, $lte: endDate },
     });
-    const balance = transactions.reduce(
-      (acc, trans) =>
-        trans.type === "income" ? acc + trans.amount : acc - trans.amount,
-      0
-    );
-    return { transactions, balance };
-  } catch (e) {
-    console.log(e);
+
+    // Obliczanie sumy wydatków
+    const expenses = transactions
+      .filter((t) => t.type === "expense")
+      .reduce((acc, trans) => acc + trans.amount, 0);
+
+    // Obliczanie sumy dochodów
+    const incomes = transactions
+      .filter((t) => t.type === "income")
+      .reduce((acc, trans) => acc + trans.amount, 0);
+
+    // Zwracanie raportów
+    return { year, month, expenses, incomes };
+  } catch (error) {
+    console.error("Error fetching reports from database:", error);
+    throw error;
   }
 };
 
+// Funkcja fetchReports do pobierania raportów
 const fetchReports = async (req, res, next) => {
   const { year, month } = req.query;
   try {
     const reports = await getReportsFromDatabase(year, month);
+
     if (!reports) {
       return res.status(404).json({ message: "Reports not found" });
     }
+
     return res.status(200).json(reports);
   } catch (error) {
     console.error("Error in fetchReports:", error);
@@ -47,6 +65,22 @@ const expenseCategories = async (_req, res, next) => {
     return res.status(200).json(expenseCategorie);
   } catch (e) {
     next(e);
+  }
+};
+
+const getPeriodData = async (startDate, endDate) => {
+  try {
+    const transactions = await Transaction.find({
+      date: { $gte: new Date(startDate), $lte: new Date(endDate) },
+    });
+    const balance = transactions.reduce(
+      (acc, trans) =>
+        trans.type === "income" ? acc + trans.amount : acc - trans.amount,
+      0
+    );
+    return { transactions, balance };
+  } catch (e) {
+    console.log(e);
   }
 };
 
