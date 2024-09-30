d80c5b70bb7a7509ddc8398951ed08f137a5d5a9
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-09-30 22:37:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
